cbec91c205ed6110d28c58ba2c1ad16182c9bc5c
Luis 🦎 <luis.6496@hotmail.com>
2022-12-23 18:23:47 -0600

    Groovy - two-fer - refactor

--- groovy/two-fer/src/main/groovy/TwoFer.groovy
+++ groovy/two-fer/src/main/groovy/TwoFer.groovy
@@ -1,6 +1,5 @@
 class TwoFer {
   static String twoFer(String name) {
-    if(name) "One for ${name}, one for me."
-    else "One for you, one for me."
+    "One for ${name ?: 'you'}, one for me."
   }
 }
